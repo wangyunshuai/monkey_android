9fd3be9d4058f1467f6e3f44d1bba68246aba511
王云帅 <wangyunshuai@aca80f27.ipt.aol.com>
2016-08-09 18:58:14 +0800

    create

---
+++ performance/libs/device_monitor.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python
+# coding: utf-8
+
+from __future__ import print_function
+import time
+import os, sys
+import logging
+
+if __name__ == '__main__':
+    project_dir = os.path.split(os.getcwd())
+    project_dir = os.path.split(project_dir[0])
+    sys.path.append(project_dir[0])
+    os.chdir(project_dir[0])    # 将项目所在文件夹设置为 wkdir (目录下必须有 __init__.py 文件)
+
+from performance.config.config import Config
+from performance.libs.base import get_device_info
+from performance.monkey.monkey import create_threads_monkey
+
+def monitor_device():
+    while True:
+        # logging.info("monitor_device")
+        time.sleep(5)
+        current_device_dict = {}
+        new_device_dict = {}
+        current_device_dict = get_device_info()
+        if current_device_dict != Config.device_dict:
+            for device_model, device_id in current_device_dict.iteritems():
+                if device_model not in Config.device_dict:
+                    new_device_dict.update({device_model: device_id})
+            if new_device_dict != {}:
+                logging.info("found new device")
+            else:
+                logging.info('device lost')
+            Config.device_dict = current_device_dict
+            create_threads_monkey(new_device_dict)
+            # restart_threads()
+
+if __name__ == '__main__':
+    pass
+
+
---
+++ performance/monkey/monkey_server.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python
+# coding: utf-8
+
+import os
+import sys
+import logging
+import traceback
+
+if __name__ == '__main__':
+    project_dir = os.path.split(os.getcwd())
+    project_dir = os.path.split(project_dir[0])
+    sys.path.append(project_dir[0])
+    os.chdir(project_dir[0])    # 将项目所在文件夹设置为 wkdir (目录下必须有 __init__.py 文件)
+
+from performance.config.config import Config
+from performance.libs.device_monitor import monitor_device
+
+config = Config()
+
+def run():
+    logging.info('start')
+    monitor_device()
+
+if __name__ == '__main__':
+    try:
+        pass
+        run()
+    except Exception:
+        traceback.print_exc()
+    finally:
+        pass
